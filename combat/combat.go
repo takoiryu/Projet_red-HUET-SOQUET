1bd46f7932ce6576e1a293f832970e39050c26f9
morgan soquet <morgan.soquet@ynov.com>
2026-09-21 11:19:57 +0200

    test

--- combat/combat.go
+++ combat/combat.go
@@ -1,1 +1,23 @@
 package combat
+
+import (
+	"Projet_red/character"
+	"Projet_red/monstres"
+	"fmt"
+)
+
+func goblinPattern(joueur *character.Character, gobelin *monstres.Goblins, tour int) {
+	degats := gobelin.Att
+
+	if tour%3 == 0 {
+		degats = gobelin.Att * 2
+	}
+
+	joueur.Pv -= degats
+	if joueur.Pv < 0 {
+		joueur.Pv = 0
+	}
+
+	fmt.Printf("%s inflige à %s %d de dégâts !\n", gobelin.Nom, joueur.Nom, degats)
+	fmt.Printf("Santé de %s : %d/%d PV\n", joueur.Nom, joueur.Pv, joueur.Pvmax)
+}
