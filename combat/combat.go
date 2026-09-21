c746f3b3ff9294f315a368333cf0d0f3d60cb7dc
morgan soquet <morgan.soquet@ynov.com>
2026-09-21 13:52:39 +0200

    poule

--- combat/combat.go
+++ combat/combat.go
@@ -8,16 +8,46 @@ import (
 
 func goblinPattern(joueur *character.Character, gobelin *monstres.Goblins, tour int) {
 	degats := gobelin.Att
-
 	if tour%3 == 0 {
 		degats = gobelin.Att * 2
 	}
-
 	joueur.Pv -= degats
 	if joueur.Pv < 0 {
 		joueur.Pv = 0
 	}
-
 	fmt.Printf("%s inflige à %s %d de dégâts !\n", gobelin.Nom, joueur.Nom, degats)
 	fmt.Printf("Santé de %s : %d/%d PV\n", joueur.Nom, joueur.Pv, joueur.Pvmax)
+}
+
+func characterTurn(joueur *character.Character, gobelin *monstres.Goblins) {
+	var choix string
+
+	for {
+		fmt.Println("\n===== TOUR DE JOUEUR =====")
+		fmt.Println("1 : Attaquer")
+		fmt.Println("2 : Inventaire")
+		fmt.Print("Choix : ")
+		fmt.Scan(&choix)
+
+		switch choix {
+		case "1":
+			// Attaque basique (5 dégâts)
+			degats := 5
+			gobelin.Pv -= degats
+			if gobelin.Pv < 0 {
+				gobelin.Pv = 0
+			}
+
+			fmt.Printf("\n%s utilise Attaque basique et inflige %d dégâts à %s !\n", joueur.Nom, degats, gobelin.Nom)
+			fmt.Printf("PV restants de %s : %d/%d\n", gobelin.Nom, gobelin.Pv, gobelin.Pvmax)
+			return // Fin du tour du joueur
+
+		case "2":
+			fmt.Println("\n[Inventaire]")
+			// On continue la boucle pour que le joueur puisse choisir d'attaquer
+
+		default:
+			fmt.Println("\nChoix invalide ! Veuillez saisir 1 ou 2.")
+		}
+	}
 }
